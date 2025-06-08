a9ba8968cbad17f074bb4f33cd5715ec92e05955
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 15:25:32 +0800

    Update jsonstore.go

--- jsonstore/jsonstore.go
+++ jsonstore/jsonstore.go
@@ -2,45 +2,79 @@ package jsonstore
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
-	"sync"
 
 	"go.mau.fi/whatsmeow/store"
+	"go.mau.fi/whatsmeow/store/legacy"
+	"go.mau.fi/whatsmeow/store/mem"
+	waTypes "go.mau.fi/whatsmeow/types"
 )
 
-type JSONStore struct {
-	mu    sync.Mutex
-	Store *store.Device
-	Path  string
+type JSONSessionStore struct {
+	filePath string
+	data     *store.Device
+	memStore *mem.Store
 }
 
-func NewJSONStore(path string) (*JSONStore, error) {
-	js := &JSONStore{Path: path}
-	data, err := os.ReadFile(path)
-	if err == nil {
-		var dev store.Device
-		err := json.Unmarshal(data, &dev)
-		if err != nil {
-			return nil, err
-		}
-		js.Store = &dev
+func NewJSONStore(filePath string) (*JSONSessionStore, error) {
+	memStore := mem.New()
+	js := &JSONSessionStore{
+		filePath: filePath,
+		memStore: memStore,
 	}
-	if js.Store == nil {
-		js.Store = &store.Device{}
+
+	err := js.load()
+	if err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("load session error: %w", err)
 	}
 	return js, nil
 }
 
-func (j *JSONStore) Save() error {
-	j.mu.Lock()
-	defer j.mu.Unlock()
-	data, err := json.MarshalIndent(j.Store, "", "  ")
+// Memuat device dari file JSON
+func (s *JSONSessionStore) load() error {
+	file, err := os.Open(s.filePath)
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(j.Path, data, 0600)
+	defer file.Close()
+
+	var session legacy.Device
+	err = json.NewDecoder(file).Decode(&session)
+	if err != nil {
+		return fmt.Errorf("decode error: %w", err)
+	}
+
+	device := store.NewDevice(s.memStore, waTypes.DeviceID{})
+	err = device.Restore(&session)
+	if err != nil {
+		return fmt.Errorf("restore error: %w", err)
+	}
+
+	s.data = device
+	return nil
 }
 
-func (j *JSONStore) GetDevice() *store.Device {
-	return j.Store
+// Simpan session ke file
+func (s *JSONSessionStore) Save() error {
+	if s.data == nil {
+		return fmt.Errorf("device is nil")
+	}
+
+	session := s.data.Serialize()
+	file, err := os.Create(s.filePath)
+	if err != nil {
+		return fmt.Errorf("file create error: %w", err)
+	}
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(session)
+}
+
+func (s *JSONSessionStore) GetDevice() *store.Device {
+	return s.data
+}
+
+func (s *JSONSessionStore) SetDevice(device *store.Device) {
+	s.data = device
 }
