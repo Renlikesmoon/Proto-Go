68b6e444183b8e3f3e7dce8f86eb10aacc164e98
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 15:12:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
+	"github.com/mdp/qrterminal/v3"
 	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/store/mem"
+	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 
-	"go.mau.fi/whatsmeow/types/events"
-	"github.com/mdp/qrterminal/v3"
+	"github.com/Renlikesmoon/Proto-Go/jsonstore"
 )
 
 var Client *whatsmeow.Client
-var sessionFile = "session.json"
 
 func main() {
 	err := StartClient()
@@ -28,19 +26,12 @@ func StartClient() error {
 	ctx := context.Background()
 	dbLog := waLog.Noop
 
-	// In-memory store
-	store := mem.NewStore()
-
-	// Buat client kosong untuk awal
-	Client = whatsmeow.NewClient(store.NewDevice(), dbLog)
-
-	// Coba load session dari file JSON
-	if _, err := os.Stat(sessionFile); err == nil {
-		err := LoadSession()
-		if err != nil {
-			return fmt.Errorf("❌ Gagal load session: %w", err)
-		}
+	js, err := jsonstore.NewJSONStore("session.json")
+	if err != nil {
+		return err
 	}
+
+	Client = whatsmeow.NewClient(js.GetDevice(), dbLog)
 
 	Client.AddEventHandler(func(evt interface{}) {
 		switch v := evt.(type) {
@@ -59,19 +50,14 @@ func StartClient() error {
 		qrterminal.GenerateHalfBlock(resp, qrterminal.L, os.Stdout)
 		fmt.Println("✅ Scan QR di atas dengan WhatsApp kamu.")
 
-		// Tunggu sampai paired sebelum simpan session
 		Client.AddEventHandler(func(evt interface{}) {
 			if _, ok := evt.(*events.PairSuccess); ok {
-				err := SaveSession()
-				if err != nil {
-					fmt.Println("❌ Gagal simpan session:", err)
-				} else {
-					fmt.Println("💾 Session disimpan ke", sessionFile)
-				}
+				js.Save()
+				fmt.Println("💾 Session disimpan ke session.json")
 			}
 		})
 	} else {
-		err := Client.Connect()
+		err = Client.Connect()
 		if err != nil {
 			return fmt.Errorf("❌ Gagal konek ke WhatsApp: %w", err)
 		}
@@ -79,22 +65,4 @@ func StartClient() error {
 	}
 
 	return nil
-}
-
-// Simpan session ke file JSON
-func SaveSession() error {
-	data, err := Client.Store.(*mem.Device).Marshal()
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(sessionFile, data, 0600)
-}
-
-// Load session dari file JSON
-func LoadSession() error {
-	data, err := os.ReadFile(sessionFile)
-	if err != nil {
-		return err
-	}
-	return Client.Store.(*mem.Device).Unmarshal(data)
 }
