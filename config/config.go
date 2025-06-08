fa601be347db51bdd611907a9e75573596365b63
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 12:43:58 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,6 +2,6 @@ package config
 
 // OwnerJID format: 6281234567890@s.whatsapp.net
 // Ganti sesuai nomor WhatsApp owner
-const OwnerJID = "6281234567890@s.whatsapp.net"
+const OwnerJID = "6285954540177@s.whatsapp.net"
 
 const CommandPrefix = "."
