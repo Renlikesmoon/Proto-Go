6d2308c85649dc0cec1cecc783429c6d6f14920a
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 15:48:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,36 +8,34 @@ import (
 	"syscall"
 
 	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
 func main() {
 	logger := waLog.Noop
-	client := whatsmeow.NewClient(nil, logger)
+	client := whatsmeow.NewClient(waLog.Stdout("Client"), logger)
 
-	client.AddEventHandler(func(evt interface{}) {
-		switch v := evt.(type) {
-		case *events.Message:
-			fmt.Println("Pesan dari:", v.Info.Sender.String())
-		case *events.Disconnected:
-			fmt.Println("Terputus dari WhatsApp")
-		}
-	})
-
-	err := client.Connect()
+	// Jangan langsung connect, tapi pair phone atau scan QR
+	qrChan, err := client.GetQRChannel(context.Background())
 	if err != nil {
-		fmt.Println("Gagal konek:", err)
+		fmt.Println("Gagal mendapatkan QR channel:", err)
 		return
 	}
 
-	qrChan, _ := client.GetQRChannel(context.Background())
-	for evt := range qrChan {
-		if evt.Event == "code" {
-			fmt.Println("Scan QR dengan WhatsApp kamu:", evt.Code)
-		} else {
-			fmt.Println("QR error:", evt.Code)
+	go func() {
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				fmt.Println("Scan QR dengan WhatsApp kamu:", evt.Code)
+			} else {
+				fmt.Println("QR error:", evt.Code)
+			}
 		}
+	}()
+
+	err = client.Connect()
+	if err != nil {
+		fmt.Println("Gagal konek:", err)
+		return
 	}
 
 	// Tunggu Ctrl+C untuk exit
