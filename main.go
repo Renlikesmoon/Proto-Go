25102eec65e5fce8bd81ed3586bfed985ae5bec7
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 10:18:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,54 +1,18 @@
 package main
 
 import (
-    "log"
-
-    "go.mau.fi/whatsmeow"
-    "go.mau.fi/whatsmeow/store/sqlstore"
-    _ "modernc.org/sqlite"
-
-    "github.com/mdp/qrterminal/v3"
-    "context"
+	"log"
+	"os"
+	"whatsbot/lib"
 )
 
 func main() {
-    dbLog := log.Default()
-    container, err := sqlstore.New("sqlite", "file:session.db?_foreign_keys=on", dbLog)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    deviceStore, err := container.GetFirstDevice()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    client := whatsmeow.NewClient(deviceStore, nil)
-
-    if client.Store.ID == nil {
-        qrChan, _ := client.GetQRChannel(context.Background())
-        err = client.Connect()
-        if err != nil {
-            log.Fatal(err)
-        }
-
-        for evt := range qrChan {
-            if evt.Event == "code" {
-                qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, log.Writer())
-            } else {
-                log.Println("QR Event:", evt.Event)
-            }
-        }
-    } else {
-        err = client.Connect()
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
-
-    client.AddEventHandler(func(evt interface{}) {
-        messageHandler(evt, client)
-    })
-
-    select {} // block
+	if len(os.Args) < 2 {
+		log.Fatal("❗ Masukkan nomor HP untuk pairing (contoh: go run main.go 6281234567890)")
+	}
+	phone := os.Args[1]
+	err := lib.StartWhatsAppWithCode(phone)
+	if err != nil {
+		log.Fatal("❌ Error:", err)
+	}
 }
