3a6f64f2429e04a8243d6d6fde304d9ba1528d11
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 15:36:38 +0800

    Update jsonstore.go

--- jsonstore/jsonstore.go
+++ jsonstore/jsonstore.go
@@ -1,64 +1,28 @@
 package jsonstore
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-
-	"go.mau.fi/whatsmeow/store"
 )
 
 type JSONStore struct {
 	filePath string
-	store    *store.Store
 }
 
-func NewJSONStore(filePath string) (*JSONStore, error) {
-	js := &JSONStore{
-		filePath: filePath,
-	}
+func NewJSONStore(filePath string) *JSONStore {
+	return &JSONStore{filePath}
+}
 
-	err := js.Load()
-	if err != nil && !os.IsNotExist(err) {
+// Save menerima data session []byte hasil client.Store.Serialize()
+func (js *JSONStore) Save(data []byte) error {
+	return os.WriteFile(js.filePath, data, 0644)
+}
+
+// Load membaca file dan mengembalikan []byte session untuk Restore()
+func (js *JSONStore) Load() ([]byte, error) {
+	data, err := os.ReadFile(js.filePath)
+	if err != nil {
 		return nil, err
 	}
-
-	return js, nil
-}
-
-func (js *JSONStore) Load() error {
-	file, err := os.Open(js.filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	s := store.NewStore()
-	err = json.NewDecoder(file).Decode(&s.Data)
-	if err != nil {
-		return fmt.Errorf("decode session json error: %w", err)
-	}
-
-	js.store = s
-	return nil
-}
-
-func (js *JSONStore) Save() error {
-	if js.store == nil {
-		return fmt.Errorf("store is nil")
-	}
-	file, err := os.Create(js.filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	return json.NewEncoder(file).Encode(js.store.Data)
-}
-
-func (js *JSONStore) GetStore() *store.Store {
-	if js.store == nil {
-		js.store = store.NewStore()
-	}
-	return js.store
+	return data, nil
 }
