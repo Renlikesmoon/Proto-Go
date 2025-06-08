ec6dbdf30b67e8b74458208c52def2eba6c75493
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 10:36:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"log"
 	"os"
-	"whatsbot/lib"
+
+	"github.com/username/Proto-Go/config"
+	"github.com/username/Proto-Go/lib"
+	"github.com/username/Proto-Go/commands"
+
+	"go.mau.fi/whatsmeow/types/events"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("❗ Masukkan nomor HP untuk pairing (contoh: go run main.go 6281234567890)")
+		log.Fatal("Usage: go run main.go 6281234567890")
 	}
 	phone := os.Args[1]
+
 	err := lib.StartWhatsAppWithCode(phone)
 	if err != nil {
-		log.Fatal("❌ Error:", err)
+		log.Fatal("Error starting WhatsApp:", err)
 	}
+
+	lib.Client.AddEventHandler(func(evt interface{}) {
+		switch v := evt.(type) {
+		case *events.Message:
+			if v.Info.MessageSource.IsFromMe {
+				return
+			}
+			commands.HandleMessage(v)
+		}
+	})
+
+	select {} // keep running
 }
