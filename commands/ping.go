02d1110fdb06e5efaec4f95c728429bfffb7e4e6
Ren Aldridge <daviddarren220@gmail.com>
2025-06-08 13:47:21 +0800

    Update ping.go

--- commands/ping.go
+++ commands/ping.go
@@ -2,21 +2,35 @@ package commands
 
 import (
 	"context"
-	"github.com/Renlikesmoon/Proto-Go/lib"
+	"fmt" // Added for error printing
 
+	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/types/events"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
+	// The 'lib' import is no longer strictly necessary within this command
+	// as the whatsmeow.Client will be passed directly to the Run method.
 )
 
+// PingCommand implements the Command interface to reply with "Pong!".
 type PingCommand struct{}
 
+// Prefix returns the command's prefix.
 func (p *PingCommand) Prefix() string {
 	return ".ping"
 }
 
-func (p *PingCommand) Run(evt *events.Message) {
+// Run executes the Ping command.
+// It now correctly accepts the whatsmeow.Client instance.
+func (p *PingCommand) Run(evt *events.Message, client *whatsmeow.Client) {
 	text := "Pong!"
-	lib.Client.SendMessage(context.Background(), evt.Info.Chat, &waProto.Message{
+	fmt.Printf("Received .ping command from %s\n", evt.Info.Sender.String())
+
+	// Use the 'client' parameter received in the Run method to send the message.
+	_, err := client.SendMessage(context.Background(), evt.Info.Chat, &waProto.Message{
 		Conversation: &text,
 	})
+
+	if err != nil {
+		fmt.Printf("❌ Gagal mengirim pesan 'Pong!': %v\n", err)
+	}
 }
